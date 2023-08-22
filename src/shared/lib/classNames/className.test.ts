0a308323a476f0b0dc4e133f9d4b927d03fda616
Ani-21 <kvikviniya.am@edu.spbstu.ru>
2023-08-22 20:31:57 +0300

    feat: add babel config

---
+++ src/shared/lib/classNames/className.test.ts
@@ -0,0 +1,35 @@
+import { classNames } from "./classNames";
+
+describe("classNames", () => {
+  test("with one param", () => {
+    expect(classNames("someClass")).toBe("someClass");
+  });
+
+  test("with all params", () => {
+    const expected = "someClass hovered class1 class2";
+    expect(
+      classNames("someClass", { hovered: true }, ["class1", "class2"])
+    ).toBe(expected);
+  });
+
+  test("with additional class", () => {
+    const expected = "someClass class1 class2";
+    expect(classNames("someClass", {}, ["class1", "class2"])).toBe(expected);
+  });
+
+  test("with mods undefined", () => {
+    const expected = "someClass className";
+    expect(classNames("someClass", { hovered: undefined }, ["className"])).toBe(
+      expected
+    );
+  });
+
+  test("with mods false", () => {
+    const expected = "someClass hovered className";
+    expect(
+      classNames("someClass", { hovered: true, scrollable: false }, [
+        "className",
+      ])
+    ).toBe(expected);
+  });
+});
